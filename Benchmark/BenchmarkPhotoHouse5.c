063fd2a96e3c3e9b59ea92592db01e823bd3ca40
EdilsonGalvao <esj.galvao@gmail.com>
2020-08-13 06:35:30 -0400

    Support Latest code

--- Benchmark/BenchmarkPhotoHouse5.c
+++ Benchmark/BenchmarkPhotoHouse5.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #define INT_MAX 2147483647
 
+unsigned char PanelChoice, BatteryChoice, ControllerChoice, InverterChoice;
+
 //Quantities of each equipment at the database below
 unsigned char PanelQuant = 5, BatteryQuant=5, ControllerQuant=5,InverterQuant=5;
-
 int Phouse = 915, Psurge = 980, Econsumption = 4880;
 float Insol = 4.92; //(kWh/m2/day CRESESB 2016)
 int Vsystem = 24;  //12V, 24V, 36V or 48V: DC Bus voltage definition
@@ -75,121 +76,122 @@ int getTotalCost()
 int optimizeCost(int cost)
 {
     int NTP, NPP, NPS, NBtotal, NBS, NBP, NPPmin, ICmin;
+    int arrang, NPmin;
     float Ecorrected, Ep, NTPmin, NPSmin1, NPSmin2, Cbank, Iscamb, NBSmin, NBPmin, Fobj;
-    unsigned char PanelChoice, BatteryChoice, ControllerChoice, InverterChoice;
+    float Pminpanels, ItotalPVpanels, VtotalPVpanels, Eb, DODdaycalc, IminDCbus, equipcost, DODmax;
 
-    __VERIFIER_assume((cost > 0));
-
-    PanelChoice = nondet_uchar();
+	PanelChoice = nondet_uchar();
     __VERIFIER_assume(PanelChoice <= (PanelQuant-1));
 
-    BatteryChoice = nondet_uchar();
+	BatteryChoice = nondet_uchar();
     __VERIFIER_assume(BatteryChoice <= (BatteryQuant-1));
 
-    ControllerChoice = nondet_uchar();
-    __VERIFIER_assume(ControllerChoice <= (ControllerQuant-1));
+	ControllerChoice = nondet_uchar();
+	__VERIFIER_assume(ControllerChoice <= (ControllerQuant-1));
 
-    InverterChoice = nondet_uchar();
-    __VERIFIER_assume(InverterChoice <= (InverterQuant-1));
+	InverterChoice = nondet_uchar();
+	__VERIFIER_assume(InverterChoice <= (InverterQuant-1));
 
-    // information from the PV Panel
-    float Ap = PanelData[PanelChoice][0];
-    float np = PanelData[PanelChoice][1];
-    int N = (int)PanelData[PanelChoice][2];     //N=number of series-connected cells
-    int NOCT = (int)PanelData[PanelChoice][3];  //Nominal Operating Cell Temperature=45°C +-2 at G=800 W/m2, light spectrum AM 1.5G, windspeed=1m/s, air temp at 20°C
-    float mii = PanelData[PanelChoice][4];      //short-circuit current temperature coefficient (A/K)
-    float miv = PanelData[PanelChoice][5];      //open-circuit voltage temperature coefficient (V/K)
-    float Iscref = PanelData[PanelChoice][6];
-    float Vocref = PanelData[PanelChoice][7];
-    float Pmref = PanelData[PanelChoice][8];
-    float Imref = PanelData[PanelChoice][9];
-    float Vmref = PanelData[PanelChoice][10];
-    float VmpNOCT = PanelData[PanelChoice][11];
-    float PanelCost = PanelData[PanelChoice][12];
+	// information from the PV Panel
+	float Ap = PanelData[PanelChoice][0];
+	float np = PanelData[PanelChoice][1];
+	int N = (int)PanelData[PanelChoice][2];    //N=number of series-connected cells
+	int NOCT = (int)PanelData[PanelChoice][3]; //Nominal Operating Cell Temperature=45°C +-2 at G=800 W/m2, light spectrum AM 1.5G, windspeed=1m/s, air temp at 20°C
+	float mii = PanelData[PanelChoice][4]; //short-circuit current temperature coefficient (A/K)
+	float miv = PanelData[PanelChoice][5]; //open-circuit voltage temperature coefficient (V/K)
+	float Iscref = PanelData[PanelChoice][6];
+	float Vocref = PanelData[PanelChoice][7];
+	float Pmref = PanelData[PanelChoice][8];
+	float Imref = PanelData[PanelChoice][9];
+	float Vmref = PanelData[PanelChoice][10];
+	float VmpNOCT = PanelData[PanelChoice][11];
+	float PanelCost = PanelData[PanelChoice][12];
 
-    //battery information
-    float nb = BatteryData [BatteryChoice][0];
-    int Vbat = (int)BatteryData [BatteryChoice][1];
-    int capacity = (int)BatteryData [BatteryChoice][2];
-    float Vbulk = BatteryData [BatteryChoice][3]; //(14.40*NBS);
-    float Vfloat = BatteryData [BatteryChoice][4]; //(13.20*NBS);
-    float BatteryCost = BatteryData [BatteryChoice][5];
+	//battery information
+	float nb = BatteryData [BatteryChoice][0];
+	int Vbat = (int)BatteryData [BatteryChoice][1];
+	int capacity = (int)BatteryData [BatteryChoice][2];
+	float Vbulk = BatteryData [BatteryChoice][3]; //(14.40*NBS);
+	float Vfloat = BatteryData [BatteryChoice][4]; //(13.20*NBS);
+	float BatteryCost = BatteryData [BatteryChoice][5];
 
-    // charge controller
-    float nc = ControllerData[ControllerChoice][0];
-    int IC = (int)ControllerData[ControllerChoice][1];
-    int VC = (int)ControllerData[ControllerChoice][2];
-    int Vmpptmin = (int)ControllerData[ControllerChoice][3];
-    float VCmax = ControllerData[ControllerChoice][4];
-    float ControllerCost = ControllerData[ControllerChoice][5];
+	// charge controller
+	float nc = ControllerData[ControllerChoice][0];
+	int IC = (int)ControllerData[ControllerChoice][1];
+	int VC = (int)ControllerData[ControllerChoice][2];
+	int Vmpptmin = (int)ControllerData[ControllerChoice][3];
+	float VCmax = ControllerData[ControllerChoice][4];
+	float ControllerCost = ControllerData[ControllerChoice][5];
 
-    //inverter
-    float ni = InverterData[InverterChoice][0];
-    int VinDC = (int)InverterData[InverterChoice][1];
-    int VoutAC = (int)InverterData[InverterChoice][2];
-    int PACref = (int)InverterData[InverterChoice][3];
-    int MAXACref = (int)InverterData[InverterChoice][4];
-    float InverterCost = InverterData[InverterChoice][5];
+	//inverter
+	float ni = InverterData[InverterChoice][0];
+	int VinDC = (int)InverterData[InverterChoice][1];
+	int VoutAC = (int)InverterData[InverterChoice][2];
+	int PACref = (int)InverterData[InverterChoice][3];
+	int MAXACref = (int)InverterData[InverterChoice][4];
+	float InverterCost = InverterData[InverterChoice][5];
 
-    //Econsumption = energy consumed in one day from the house
-    Ecorrected = Econsumption / (nb*nc*ni);
+	//Econsumption = energy consumed in one day from the house
+	Ecorrected = Econsumption / (nb*nc*ni);
 
-    //estimation of the energy of each panel (Wh)
-    Ep = Insol * Ap * np * 1000;
+	Pminpanels = 1.25 * Ecorrected / Insol;
+	NPmin = ((Pminpanels -1)/ Pmref)+1;
+	NPP = 2;
+	if ((NPmin % 2) == 0) NPS = NPmin/2; //even number
+	else NPS = (NPmin + 1)/2; //odd number
+	NTP = NPS * NPP;
 
-    //PV panel check
-    NTPmin = Ecorrected/Ep;
-    NTP = ((Ecorrected -1)/Ep) + 1; //up next integer of division
-    NPSmin1 = (Vsystem + 1) / Vmpptmin;
-    NPSmin2 = VCmax / (float)Vmref;
-    NPS = VCmax / Vmref;
-    NPPmin = (int)Phouse / (int)(NPS * Pmref);
+    //checking voltage and current from the PV panel and the controller
+    ItotalPVpanels = NPP * Imref;
+    __VERIFIER_assume ( (IC >= ItotalPVpanels) );
 
-    if (NPPmin == 0) 
-        NPP =1;
-    else 
-        NPP = (int)NPPmin;
+    VtotalPVpanels = NPS * Vmref;
+    __VERIFIER_assume ( (VCmax >= VtotalPVpanels) );
 
-    NTP = NPS * NPP;
+    DODmax = (100-SOClimit)*2;
+    autonomy = 2; // (48/24)
+    Eb = autonomy * Ecorrected*100/DODmax;
 
-    //battery check
-    Cbank = Ecorrected * autonomy / (Vsystem*(float)(100-SOClimit));
-    NBSmin = Vsystem/Vbat;
-    NBS = ((Vsystem -1)/Vbat) + 1;
-    NBPmin = Cbank / capacity;
-    NBP = ((Cbank -1)/capacity)+1;
+    DODdaycalc = Ecorrected * 100/Eb;
+
+    //check DC bus current
+    IminDCbus = Eb/(float)Vsystem;
+
+    ///////arranjo baterias
+    NBS = Vsystem/Vbat; //fixed value 
+    NBP = ((IminDCbus -1)/ capacity)+1; //calculando e arredondando
     NBtotal = NBS * NBP;
-    __VERIFIER_assume( (VC == Vsystem) );
-    Iscamb = Iscref*(1+mii*(NOCT-25)); //from each PV panel
-    ICmin = ((Iscamb - 1) * NPP)+1;
-    __VERIFIER_assume( (IC >= ICmin) );
 
-    //inverter sizing and check
-    __VERIFIER_assume ( ( VinDC >= Vsystem ) ); //if inverter supports 48V, then it supports 12 and 24 as well
+    //controller vs inverter check
+    __VERIFIER_assume ((VCmax* IC * nc) >= PACref);
 
-    __VERIFIER_assume ( (VoutAC >= (VAC-VAC*15/100)) && (VoutAC <= (VAC+VAC*15/100)) ); //Inverter output mus be +- 15% of the expected house's VAC
+	//inverter sizing and check
+	__VERIFIER_assume ( ( VinDC >= Vsystem ) ); //if inverter supports 48V, then it supports 12 and 24 as well
 
-    __VERIFIER_assume ( (Phouse <= PACref) && (Psurge <= MAXACref) ); //Power supported by the inverter must be greater than House's demand
-                                                                      // and the Peak power from inverter must support the Reak from the house
+	__VERIFIER_assume ( (VoutAC >= (VAC-VAC*15/100)) && (VoutAC <= (VAC+VAC*15/100)) ); 
+	//Inverter output mus be +- 15% of the expected house's VAC
 
-    // Function objective: minimum acquisition cost
-    Fobj= NTP*PanelCost + NBtotal*BatteryCost + ControllerCost + InverterCost;
-    LCC= Fobj+Fobj*0.05+20*289.64; //cost = equipment + 5% of installation + 3*batrep + O&M cost [out: 3*(NBtotal*BatteryCost)+]
-    __VERIFIER_assume ( ( Fobj >= (cost-500) ) );
+	__VERIFIER_assume ( (Phouse <= PACref) && (Psurge <= MAXACref) ); 
+	//Power supported by the inverter must be greater than House's demand
+	// and the Peak power from inverter must support the Peak from the house
 
-    //minimize cost of the solution, considering all the equipment that can be used and was declared at the code
-    if (!(Fobj > cost)) { __VERIFIER_error(); }
-    
-    return 0;
+	// Function objective: minimum acquisition cost
+	Fobj= NTP*PanelCost + NBtotal*BatteryCost + ControllerCost + InverterCost;
+	LCC = Fobj+Fobj*0.05+20*289.64; //cost = equipment + 5% of installation + 3*batrep + O&M cost [out: 3*(NBtotal*BatteryCost)+]
+	//minimize cost of the solution, considering all the equipment that can be used and was declared at the code
+	
+    if (!(Fobj > (float)cost)) { __VERIFIER_error(); }
+
+	return 0;
 }
 
 int main()
 {
-    int total = getTotalCost();
+    // int total = getTotalCost();
 
-    for (int i = 0; i < total * 5; i++)
+    for (int i = 461; i < 461 * 5; i++)
     {
-        optimizeCost(total+ i);
+        optimizeCost(i);
     }    
 
     return 0;
