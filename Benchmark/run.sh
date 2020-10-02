8cd6e1dd245538bf9e0274a0809774fc7441aa18
EdilsonGalvao <esj.galvao@gmail.com>
2020-10-01 21:20:03 -0400

    Updated Vz Runner

--- Benchmark/run.sh
+++ Benchmark/run.sh
@@ -1,33 +1,14 @@
-#!/bin/bash
-TM=36000
-
+#!/bin/bash	#!/bin/bash
+TM=36000	TM=36000
+echo ----------------- Z3 -----------------	
 echo ----------------- ESBMC -----------------
-echo --------------------01-------------------
-date
-timeout $TM ./esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC01_opt_PVcode.c --incremental-bmc --boolector> case01.txt
-date
-echo --------------------02-------------------
-date
-timeout $TM ./esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC02_opt_PVcode.c --incremental-bmc --boolector> case01.txt
-date
-echo --------------------03-------------------
-date
-timeout $TM ./esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC03_opt_PVcode.c --incremental-bmc --boolector> case01.txt
-date
-echo --------------------04-------------------
-date
-timeout $TM ./esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC04_opt_PVcode.c --incremental-bmc --boolector> case01.txt
-date
-echo --------------------05-------------------
-date
-timeout $TM ./esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC05_opt_PVcode.c --incremental-bmc --boolector> case01.txt
-date
-echo --------------------06-------------------
-date
-timeout $TM ./esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC06_opt_PVcode.c --incremental-bmc --boolector> case01.txt
-date
-echo --------------------07-------------------
-date
-timeout $TM ./esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC07_opt_PVcode.c --incremental-bmc --boolector> case01.txt
-date
-echo 
+echo --------------------------------------	echo --------------------------------------
+timeout $TM dotnet run --project ./Benchmark/vZ/ 1
+#timeout $TM dotnet run --project ./Benchmark/vZ/ 2	
+#timeout $TM dotnet run --project ./Benchmark/vZ/ 3	
+#timeout $TM dotnet run --project ./Benchmark/vZ/ 4	
+#timeout $TM dotnet run --project ./Benchmark/vZ/ 5	
+#timeout $TM dotnet run --project ./Benchmark/vZ/ 6	
+#timeout $TM dotnet run --project ./Benchmark/vZ/ 7	
+echo	echo
+echo
