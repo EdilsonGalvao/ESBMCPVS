7f23ec113c1007d9142a1be697d34e0a652291f8
EdilsonGalvao <esj.galvao@gmail.com>
2020-10-01 21:31:29 -0400

    Fixed Path

--- Benchmark/ESBMC/run.sh
+++ Benchmark/ESBMC/run.sh
@@ -4,30 +4,30 @@ TM=36000
 echo ----------------- ESBMC -----------------
 echo --------------------01-------------------
 date
-timeout $TM ../../../esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC01_opt_PVcode.c --incremental-bmc --boolector> case01.txt
+timeout $TM ../../../esbmc/v6.4.0/bin/esbmc TC01_opt_PVcode.c --incremental-bmc --boolector> case01.txt
 date
 echo --------------------02-------------------
 date
-timeout $TM ../../../esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC02_opt_PVcode.c --incremental-bmc --boolector> case02.txt
+timeout $TM ../../../esbmc/v6.4.0/bin/esbmc TC02_opt_PVcode.c --incremental-bmc --boolector> case02.txt
 date
 echo --------------------03-------------------
 date
-timeout $TM ../../../esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC03_opt_PVcode.c --incremental-bmc --boolector> case03.txt
+timeout $TM ../../../esbmc/v6.4.0/bin/esbmc TC03_opt_PVcode.c --incremental-bmc --boolector> case03.txt
 date
 echo --------------------04-------------------
 date
-timeout $TM ../../../esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC04_opt_PVcode.c --incremental-bmc --boolector> case04.txt
+timeout $TM ../../../esbmc/v6.4.0/bin/esbmc TC04_opt_PVcode.c --incremental-bmc --boolector> case04.txt
 date
 echo --------------------05-------------------
 date
-timeout $TM ../../../esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC05_opt_PVcode.c --incremental-bmc --boolector> case05.txt
+timeout $TM ../../../esbmc/v6.4.0/bin/esbmc TC05_opt_PVcode.c --incremental-bmc --boolector> case05.txt
 date
 echo --------------------06-------------------
 date
-timeout $TM ../../../esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC06_opt_PVcode.c --incremental-bmc --boolector> case06.txt
+timeout $TM ../../../esbmc/v6.4.0/bin/esbmc TC06_opt_PVcode.c --incremental-bmc --boolector> case06.txt
 date
 echo --------------------07-------------------
 date
-timeout $TM ../../../esbmc/v6.4.0/bin/esbmc ./ESBMCPVS/Benchmark/ESBMC/TC07_opt_PVcode.c --incremental-bmc --boolector> case07.txt
+timeout $TM ../../../esbmc/v6.4.0/bin/esbmc TC07_opt_PVcode.c --incremental-bmc --boolector> case07.txt
 date
 echo 
